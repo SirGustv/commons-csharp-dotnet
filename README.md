a4a9f4f4451bda27a1208687560bdcbd2d279c11
Gustavo <gust.bersan@gmail.com>
2022-03-04 22:16:23 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+## DESCRIÇÃO
+
+Repositório dedicado para colocar códigos, funções e instruções utilitárias sobre .NET e C# para fins de estudo e fixação.
---
+++ comandos_dotnet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace comandos_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+    }
+}
